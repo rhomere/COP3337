bcdf0e7421b640a2fe78b5a9a4d62b32d3f3d1da
Renel Homere <rhomere@gmail.com>
2021-06-20 15:49:29 -0400

    add chapter_16.java

---
+++ Zybooks/src/Chapter_16.java
@@ -0,0 +1,45 @@
+
+public class Chapter_16 {
+	public static void Run() {
+//		  double myDouble1;
+//	      double myDouble2;
+//	
+//	      myDouble1 = 93.9214;
+//	      myDouble2 = 12.3423;
+//	
+//	      System.out.printf("%.2f\n", myDouble1);
+//	      System.out.printf("%.6f\n", myDouble2);
+		
+//		double myDouble;
+//
+//	      myDouble = 93.9214;
+//
+//	      System.out.printf("%e\n", myDouble);
+		
+//		int myInt;
+//	      double myDouble;
+//
+//	      myInt = 784;
+//	      myDouble = 31.54;
+//
+//	      System.out.printf("%04d\n", myInt);
+//	      System.out.printf("%06.2f\n", myDouble);	
+		
+//		String myString;
+//
+//	      myString = "Harry";
+//
+//	      System.out.printf("%3s\n", myString);
+//	      System.out.printf("%6s\n", myString);
+		
+		String name1;
+	      String name2;
+
+	      name1 = "Layla";
+	      name2 = "Ann";
+
+	      System.out.printf("%-7s*\n", name1);
+	      System.out.printf("%-7s*\n", name2);
+	}
+
+}
